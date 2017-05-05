7277e3a3014a90d03b7cc045634f9748650aaf24
John Paul Gallagher <johnpaulgall@gmail.com>
2017-05-05 14:13:50 -0400

    Thesis commit
    All of it

---
+++ main.js
@@ -0,0 +1,34 @@
+var main = function() {
+  $('.arrow-next').click(function() {
+    var currentItem = $('.active-slide');
+    var nextItem = currentItem.next();  
+
+  if($(nextItem).hasClass( "slide" )) { 
+    if(nextItem.length == 0) {
+      nextItem = $('.slide').first();
+    }
+    
+    currentItem.removeClass('active-slide');
+    nextItem.addClass('active-slide');
+    }  
+  });
+
+  
+  $('.arrow-prev').click(function() {
+    var currentItem = $('.active-slide');
+    var prevItem = currentItem.prev();
+
+    if($(prevItem).hasClass( "slide" )) { 
+      if (prevItem.length == 0) {
+      prevItem = $('.slide').last();
+        }
+    
+    currentItem.removeClass('active-slide');
+    prevItem.addClass('active-slide');
+    }
+  });
+
+
+};
+
+$(document).ready(main);
